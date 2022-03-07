33aebf35fe51c89ced1afacf7221b5213a0a7ca3
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-03-07 19:04:34 -0300

    Requisito 2: Tem um warning que não consigui resolver

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { createUser } from '../services/userAPI';
+
+import Loading from './Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loginName: '',
+      btnDisabled: true,
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minLength = 3;
+    this.setState({
+      loginName: value,
+      btnDisabled: value.length < minLength,
+    });
+  };
+
+  handleUserLogin = (event) => {
+    event.preventDefault();
+
+    const { loginName } = this.state;
+
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name: loginName });
+      this.setState({
+        loading: false,
+        redirect: true,
+      });
+    });
+  }
+
   render() {
+    const { loginName, btnDisabled, loading, redirect } = this.state;
+
     return (
       <div
         data-testid="page-login"
       >
-        Login
+
+        {loading
+          ? <Loading />
+          : (
+            <form>
+              <label htmlFor="login-name">
+                Login
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  name="loginName"
+                  id="login-name"
+                  placeholder="Nome"
+                  value={ loginName }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="submit"
+                disabled={ btnDisabled }
+                onClick={ this.handleUserLogin }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        {redirect && <Redirect to="/search" />}
       </div>
     );
   }
